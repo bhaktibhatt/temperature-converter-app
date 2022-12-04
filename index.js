6fbecbb8a4e64cbafa4f375e10921c5c440c58ff
Bhakti <bhaktinirbhaybhatt@gmail.com>
2022-12-04 17:57:57 +0530

    css added

--- index.js
+++ index.js
@@ -6,17 +6,17 @@ function ConvertTemp(){
     let unitSelector = document.getElementById('tempUnit').value;
 
     if (unitSelector == 1){
-        let farenheit = (degreeTemp*9/5)+32;
-        let farenheitTemp = farenheit.toFixed(3);
-        console.log("Farenheit to celcius:"+farenheitTemp);
-        convertedTemp.innerHTML = farenheitTemp+ '°F';
-    }
-
-    else if (unitSelector == 2){
         let celcius = (degreeTemp-32)*(5/9); 
         let celciusTemp = celcius.toFixed(3);
         console.log("Celcius to farenheit:"+ celciusTemp)
         convertedTemp.innerHTML = celciusTemp+ '°C';
+    }
+    
+    else if (unitSelector == 2){
+        let farenheit = (degreeTemp*9/5)+32;
+        let farenheitTemp = farenheit.toFixed(3);
+        console.log("Farenheit to celcius:"+farenheitTemp);
+        convertedTemp.innerHTML = farenheitTemp+ '°F';
     }
 }
 
