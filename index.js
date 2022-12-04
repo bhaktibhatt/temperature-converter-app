d12e2090ac6a483b991462b002d2206c0d8f7f9a
Bhakti <bhaktinirbhaybhatt@gmail.com>
2022-12-04 17:51:40 +0530

    css added

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 
-function Celcius(){
+
+function ConvertTemp(){
     let degreeTemp = document.getElementById("temperatureVal").value;
-    console.log(degreeTemp.value);
-    let  farenheitTemp = (((Number(degreeTemp))-32)*5)/9;
-    console.log("Farenheit to celcius:"+farenheitTemp);
-    document.getElementById("degResult").innerHTML = farenheitTemp;
+    let convertedTemp = document.getElementById("degResult")
+    let unitSelector = document.getElementById('tempUnit').value;
+
+    if (unitSelector == 1){
+        let farenheit = (degreeTemp*9/5)+32;
+        let farenheitTemp = farenheit.toFixed(3);
+        console.log("Farenheit to celcius:"+farenheitTemp);
+        convertedTemp.innerHTML = farenheitTemp+ '°F';
+    }
+
+    else if (unitSelector == 2){
+        let celcius = (degreeTemp-32)*(5/9); 
+        let celciusTemp = celcius.toFixed(3);
+        console.log("Celcius to farenheit:"+ celciusTemp)
+        convertedTemp.innerHTML = celciusTemp+ '°C';
+    }
 }
+
